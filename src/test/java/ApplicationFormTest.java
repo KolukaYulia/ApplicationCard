33da0483a05ba7c0594c2de496238ce802715ac4
“Koluka” <koluka78@mail.ru>
2023-05-03 22:10:07 +0300

    AddTestClass

---
+++ src/test/java/ApplicationFormTest.java
@@ -0,0 +1,40 @@
+import org.jsoup.helper.W3CDom;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static java.lang.System.setProperty;
+
+
+public class ApplicationFormTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setupAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+    @BeforeEach
+    void setup() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void teardown() {
+        driver.quit();
+    }
+    @Test
+    void applicationFormTest() {
+    }
+
+}
+
